306e79ac2221175cdeb927d863ca9fbecdf30aeb
jaketysome <jaketysome@gmail.com>
2025-05-16 08:58:55 +0100

    refactors countWords

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,13 +1,5 @@
 export function countWords(str: string): number {
-  let count = 0;
-  const words = str.split(/\s+/);
-  words.forEach((word) => {
-    if (word !== "") {
-      count++;
-    }
-  });
-
-  return count;
+  return str.split(/\s+/).filter((word) => word !== "").length;
 }
 
 export function extractWords(str: string): string[] {
