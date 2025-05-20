5a85575f3c7091deca9de063fcf3211b4097d426
jaketysome <jaketysome@gmail.com>
2025-05-20 11:22:27 +0100

    adds prompt to win/lose messages

--- app/components/notify.tsx
+++ app/components/notify.tsx
@@ -4,28 +4,29 @@ import { toast, ToastContainer } from "react-toastify";
 import { useGlobalStateContext } from "../context/global-state-context";
 import { useEffect } from "react";
 
-function Message({ data }: { data: { title: string } }) {
+function Message({ data }: { data: { title: string; text: string } }) {
   return (
     <div className='msg-container'>
       <p className='msg-title'>{data.title}</p>
+      <p className='msg-description mt-2 font-bold'>{data.text}</p>
     </div>
   );
 }
 
 function Notify() {
-  const { winCondition, loseCondition } = useGlobalStateContext();
+  const { prompt, winCondition, loseCondition } = useGlobalStateContext();
 
   const notifyWin = () => {
     toast(Message, {
       position: "top-center",
-      data: { title: "🏆 You Win!" },
+      data: { title: "🏆 You Win!", text: prompt },
     });
   };
 
   const notifyLose = () => {
     toast(Message, {
       position: "top-center",
-      data: { title: "😞 You Lose!" },
+      data: { title: "😞 You Lose!", text: prompt },
     });
   };
 
